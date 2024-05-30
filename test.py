64efa1e00f878d28e91c6ff89b45a3c81f71c0a2
Anshuman Thakur <77040583+anshuman2197@users.noreply.github.com>
2024-05-30 20:18:38 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,55 @@
+# for i in range(0,4):
+#     for j in range(0,4):
+#         print("*",end="")
+#     print()
+
+# for i in range(0,5):
+#     for j in range(i):
+#         print("*", end=" ")
+
+#     print()
+
+# n=5
+# for i in range(1,n+5):
+#     for j in range(1,i+1):
+#         print(j,end=" ")
+
+#     print()
+
+# n=5
+# for i in range(1,n+5):
+#     for j in range(1,i+1):
+#         print(i,end=" ")
+
+#     print()
+
+# for i in range(0,5):
+#     for j in range(5-i):
+#         print("*",end=" ")
+#     print()
+
+# n=6
+# for i in range(1,n+6):
+#     for j in range(1,6-i):
+#         print(j,end=" ")
+
+#     print()
+# n=5
+# for i in range(n):
+#     # Calculate the number of spaces and asterisks for the current row
+#     spaces = ' ' * (n - i - 1)
+#     stars = '*' * (2 * i + 1)
+    
+#     # Print the current row with spaces followed by asterisks
+#     print(spaces + stars)
+ 
+# n=7
+# for i in range(n):
+    
+#     space=' '*i
+#     stars='*' *(2*(n-i)-1)
+
+#     print(space+stars)
+
+
+
